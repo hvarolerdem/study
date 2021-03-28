4f306da2354bb6543fcbc07f3bafa7b1f4305e7e
hvarolerdem <huseyinvarolerdem@gmail.com>
2021-03-28 23:06:06 +0300

    reverse_method

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from text_operations import split_text_into_list, merge_two_text 
+from text_operations import split_text_into_list, merge_two_text, reverse_text
 
 if __name__ == "__main__":
 
     print(split_text_into_list("It is just study"))
     print(merge_two_text("It is just","a git study"))    
+    print(reverse_text("Hello Git"))
--- text_operations.py
+++ text_operations.py
@@ -4,3 +4,8 @@ def split_text_into_list(text):
 
 def merge_two_text(text1, text2):
     return text1 + " " + text2
+
+
+def reverse_text(text):
+    return text[::-1]  
+      
