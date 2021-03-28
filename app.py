6498e5806039e4af8b5335f3989e799c8f6994c9
hvarolerdem <huseyinvarolerdem@gmail.com>
2021-03-28 22:50:53 +0300

    second commit

---
+++ app.py
@@ -0,0 +1,6 @@
+from text_operations import split_text_into_list, merge_two_text 
+
+if __name__ == "__main__":
+
+    print(split_text_into_list("It is just study"))
+    print(merge_two_text("It is just","a git study"))    
---
+++ text_operations.py
@@ -0,0 +1,6 @@
+def split_text_into_list(text):
+    return text.split()
+
+
+def merge_two_text(text1, text2):
+    return text1 + " " + text2
